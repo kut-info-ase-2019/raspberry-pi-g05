38bdccfa4d03a68f910466c430c4415edd3dd30f
teramura <meryandhappy@gmail.com>
2019-06-29 15:47:11 +0900

    commit

--- wav2dec.py
+++ wav2dec.py
@@ -9,6 +9,13 @@ buf = wf.readframes(wf.getnframes())
 # バイナリデータを整数型（16bit）に変換
 data = np.frombuffer(buf, dtype="int16")
 
+f = open('test.txt', 'w') # 書き込みモードで開く
+for i in data:
+	#print(i)
+	f.write(str(i) + "\n") # 引数の文字列をファイルに書き込む
+
+f.close() # ファイルを閉じる
+
 # グラフ化
 plt.plot(data)
 plt.grid()
