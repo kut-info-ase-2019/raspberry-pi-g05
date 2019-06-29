c9ce9c323d6c2a398a96aab36c7c33fbe8591ff0
teramura <meryandhappy@gmail.com>
2019-06-29 11:49:29 +0900

    make decibels measure

---
+++ wav2dec.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+
+wf = wave.open("test.wav" , "r" )
+buf = wf.readframes(wf.getnframes())
+
+# バイナリデータを整数型（16bit）に変換
+data = np.frombuffer(buf, dtype="int16")
+
+# グラフ化
+plt.plot(data)
+plt.grid()
+plt.show()
